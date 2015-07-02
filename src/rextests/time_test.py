fffec6b162e0aa1d3ba9edbef8341efc2a572c76
Mike <kjyoung@ualberta.ca>
2015-07-02 12:48:48 -0600

    added more 5x5 tests based on current results

--- src/rextests/time_test.py
+++ src/rextests/time_test.py
@@ -6,7 +6,7 @@ PASSCOLOR = '\033[92m'
 FAILCOLOR = '\033[91m'
 ENDCOLOR = '\033[0m'
 
-def testwins(program, size, opening, expected_wins, expected_loses):
+def testallwins(program, size, opening, expected_wins):
 	program.sendCommand("clear_board")
 	program.sendCommand("boardsize "+str(size))
 	toplay = 0
@@ -14,25 +14,40 @@ def testwins(program, size, opening, expected_wins, expected_loses):
 		program.sendCommand("play "+PLAYERS[toplay]+" "+move)
 		toplay = not toplay
 	wins = program.sendCommand("dfpn-solver-find-winning "+PLAYERS[toplay]).split()
-	if all(x in wins for x in expected_wins)\
-	and not any(x in wins for x in expected_loses):
+	if set(expected_wins) == set(wins):
 		return (True, [], [])
 	else:
 		false_losses = [x for x in expected_wins if x not in wins]
-		false_wins = [x for x in expected_loses if x in wins]
+		false_wins = [x for x in wins if x in expected_wins]
 		return(False, false_losses, false_wins)
 
-mohex = Program("../mohex/./mohex", True)
+mohex = Program("../mohex/./mohex 2>/dev/null", True)
 mohex.sendCommand("param_dfpn threads 4")
+with open("rex5x5opening.dat") as f:
+	lines = f.readlines()
+	cases = [[line.split(':')[0],line.split(':')[1].split()] for line in lines]
 
-name = "5x5 a1"
+fail_count = 0
 start = time.time()
-result, false_losses, false_wins = testwins(mohex, 5, ["a1"], ["b1","c1","d1","e1","a2","b2","c2","e2","a3","b3","d3","e3","a4","c4","e4","a5","b5","c5","d5","e5"], ["d2","c3","b4","d4"])
+for case in cases:
+	name = "5x5 "+case[0]
+	opening = case[0]
+	expected = case[1]
+	test_start = time.time()
+	result, false_losses, false_wins = testallwins(mohex,5,[opening],expected)
+	if result:
+		print(PASSCOLOR + name + " test passed! :)" + ENDCOLOR)
+		elapsed = time.time() - test_start
+		print("Runtime: "+str(elapsed))
+	else:
+		fail_count+=1
+		print(FAILCOLOR + name + " test failed... :(" + ENDCOLOR)
+		print("Misclassified as loss: "+str(false_losses))
+		print("Misclassified as win: "+str(false_wins)+'\n')
 elapsed = time.time() - start
-if result:
-	print(PASSCOLOR + name + " test passed! :)" + ENDCOLOR)
-	print("Runtime: "+str(elapsed))
+if(fail_count):
+	print(FAILCOLOR+str(fail_count)+" tests failed... :(" + ENDCOLOR)
 else:
-	print(FAILCOLOR + name + " test failed... :(" + ENDCOLOR)
-	print("Misclassified as loss: "+str(false_losses))
-	print("Misclassified as win: "+str(false_wins)+'\n')
+	print(PASSCOLOR+"All tests passed :)"+ENDCOLOR)
+print("Total Runtime: "+str(elapsed))
+
