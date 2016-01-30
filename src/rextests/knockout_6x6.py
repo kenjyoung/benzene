33ef0763986f020fb3136f008eaba274e3cec687
Mike <kjyoung@ualberta.ca>
2016-01-29 18:02:46 -0700

    added 6x6 knockout tests

---
+++ src/rextests/knockout_6x6.py
@@ -0,0 +1,18 @@
+import time
+from program import Program
+
+PLAYERS = {0: "white", 1: "black"}
+PASSCOLOR = '\033[92m'
+FAILCOLOR = '\033[91m'
+ENDCOLOR = '\033[0m'
+
+	mohex = Program("../mohex/./mohex 2>/dev/null", False)
+	mohex.sendCommand("param_dfpn threads 4")
+
+	mohex.sendCommand("play white d2")
+	start = time.time()
+	mohex.sendCommand("dfpn-solve-state black")
+	elapsed = time.time() - start
+print("Total Runtime: "+str(elapsed))
+
+
